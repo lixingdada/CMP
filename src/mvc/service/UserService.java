773ece92f86e0ce8af25298716521a2c18880c35
lixingdada <2170423283@qq.com>
2024-10-28 01:00:43 +0800

    c

---
+++ src/mvc/service/UserService.java
@@ -0,0 +1,19 @@
+package mvc.service;
+
+import mvc.domain.User;
+
+public class UserService {
+    private String msg;
+    public boolean login(User user) {
+        if (user.getUsername().length() > 8) {
+            this.msg = "用户名不合法";
+            return false;
+        }
+        if (user.getPassword().length() > 8) {
+            this.msg = "密码不合法";
+            return false;
+        }
+
+        return true;
+    }
+}
