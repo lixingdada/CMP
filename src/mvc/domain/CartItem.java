20e9280e5d38555c4667788424e0063727962e62
lixingdada <2170423283@qq.com>
2024-10-30 01:07:52 +0800

    cart init

---
+++ src/mvc/domain/CartItem.java
@@ -0,0 +1,48 @@
+package mvc.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class CartItem implements Serializable {
+
+    //private Item item;   //商品类型
+
+    private int quantity;   //商品数量
+    private BigDecimal total;    //总价
+
+/*     等Item类完成
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+        calculateTotal();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        calculateTotal();
+    }
+
+//计算总价
+    public void incrementQuantity() {
+        quantity++;
+        calculateTotal();
+    }
+
+    private void calculateTotal() {
+        if (item != null && item.getListPrice() != null) {
+            total = item.getListPrice().multiply(new BigDecimal(quantity));
+        } else {
+            total = null;
+        }
+    }
+
+ */
+}
