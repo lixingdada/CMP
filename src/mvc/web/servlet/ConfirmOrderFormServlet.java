985593f3efb1ab1904369cade11e6552f79ad5a4
“huangchengjie666” <2044782778@qq.com>
2024-12-20 01:21:04 +0800

    实验二的初始化（注意数据库名字做了修改）

--- src/mvc/web/servlet/ConfirmOrderFormServlet.java
+++ src/mvc/web/servlet/ConfirmOrderFormServlet.java
@@ -25,6 +25,8 @@ public class ConfirmOrderFormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        req.setCharacterEncoding("UTF-8");
+
         HttpSession session=req.getSession();
         String username = req.getParameter("username");
 
